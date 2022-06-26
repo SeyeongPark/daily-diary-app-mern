6fe99dfc8b77c4329aed6c22608fbcb037c1666a
SeyeongPark <lynn.yeong.102@gmail.com>
2022-06-26 01:56:11 -0400

    base of form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import DailyDiary from './components/Main/dailyDiary';
+import DailyDiary from './components/Main/diary';
 
 const App = () => {
   return (
--- src/components/Main/diary.js
+++ src/components/Main/diary.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './diary.css';
 
 function DailyDiary() {
 
@@ -12,12 +13,22 @@ function DailyDiary() {
           <form>
             <p>Title</p>
             <input type="text" className="title" placeholder="Title"/>
+
             <p>Date</p>
             <input id="date" type="date" name="date" defaultValue={defaultValue} />
+
             <p>Weather</p>
-            <input type="text" className="weather" placeholder="Weather"/>
+            
+            <select className="weather" id='weather'>
+              <option value="sunny">Sunny</option>
+              <option value="cloudy">Cloudy</option>
+              <option value="rainy">Rainy</option>
+              <option value="snowy">Snowy</option>
+            </select>
+
             <p>color</p>
             <input type="color" className="color"/>
+
             <p>Content</p>
             <textarea className="content" rows='20' cols='20'/>
 
